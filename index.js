093c041d40760bfce55d23a48d483107ab466b1d
iahmed93 <islam93ahmed@hotmail.com>
2022-02-16 12:03:49 +0200

    add platform config package

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const config = require("platformsh-config").config();
+const port = config.port || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello Platform.sh!");
