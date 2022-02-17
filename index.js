67106fcb0bf3e20cd7ed8ff5796717cbc227bee7
iahmed93 <islam93ahmed@hotmail.com>
2022-02-17 16:52:17 +0200

    add databse connection

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require("express");
-const app = express();
+const { Model } = require("objection");
+const Knex = require("knex");
+
 const config = require("platformsh-config").config();
+
 const port = config.port || 3000;
 
+const credentials = config.credentials("database");
+
+const knex = require("knex")({
+  client: "mysql",
+  connection: {
+    host: credentials.host,
+    port: credentials.port,
+    user: credentials.username,
+    password: credentials.password,
+    database: credentials.path,
+  },
+});
+
+Model.knex(knex);
+
+const app = express();
+
 app.get("/", (req, res) => {
-  res.send("Hello Platform.sh!");
+  res.send("Hello Platform.sh!" + credentials.path);
 });
 
 app.listen(port, () => {
