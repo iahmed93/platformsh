81e3a75d946c293690f5d743afb53ed5370fc642
iahmed93 <islam93ahmed@hotmail.com>
2022-02-20 12:50:46 +0200

    update

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const credentials = config.credentials("mysql_db");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello Platform.sh!");
+  res.send("Hello Platform.sh! " + credentials.password);
 });
 
 app.get("/test", (req, res) => {
