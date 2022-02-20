e975ab6b157e63535bf075192de22894d386dc01
iahmed93 <islam93ahmed@hotmail.com>
2022-02-20 11:27:42 +0200

    update

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 const express = require("express");
 const { Model } = require("objection");
+const log = require("simple-node-logger").createSimpleLogger("logs.log");
 
 const config = require("platformsh-config").config();
 
 const port = config.port || 3000;
 
-const credentials = config.credentials("mysql_db");
+// const credentials = config.credentials("mysql_db");
 
-const knex = require("knex")({
-  client: "mysql",
-  connection: {
-    host: credentials.host,
-    port: credentials.port,
-    user: credentials.username,
-    password: credentials.password,
-    database: credentials.path,
-  },
-});
+// const knex = require("knex")({
+//   client: "mysql",
+//   connection: {
+//     host: credentials.host,
+//     port: credentials.port,
+//     user: credentials.username,
+//     password: credentials.password,
+//     database: credentials.path,
+//   },
+// });
 
-Model.knex(knex);
+// Model.knex(knex);
 
 const app = express();
 
@@ -32,4 +33,5 @@ app.get("/test", (req, res) => {
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
+  log.info(`Listening on port ${port}`);
 });
